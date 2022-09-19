b12521e65f0ad24f7ae212ab466f22ca79421708
Matias Silva <matias.silva4@hotmail.com>
2022-09-19 20:37:57 -0300

    Inicializacio proyecto webDjango

---
+++ Integrantes/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from Integrantes.models import Integrante
+
+# Register your models here.
+
+admin.site.register(Integrante)
---
+++ Integrantes/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Integrante(models.Model):
+    nombre = models.CharField(max_length=15)
+    apellido = models.CharField(max_length=15)
+    edad = models.IntegerField()
+    nacimiento = models.DateField()
+
+    def __str__(self):
+        return self.nombre
